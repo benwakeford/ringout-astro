172cf62f8595dbad02ad185a949bfd97ecd20578
Ben Wakeford <mail@benwakeford.com>
2023-09-27 17:25:32 +0100

    update title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-slate-400">
       <div className="container py-4">
-        <h1>My App</h1>
+        <h1>Ring Out</h1>
       </div>
     </header>
   );
