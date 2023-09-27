e15102e28873945631e5b3f7dd792cba8248c73f
Ben Wakeford <mail@benwakeford.com>
2023-09-27 16:56:36 +0100

    Simple structure

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container mx-auto px-4 border-b-2 pb-4 mb-4">
+        <h1>My App</h1>
+      </div>
+    </header>
+  );
+}
