a4c0f61360abb524915fd28b8254a30ec9ace66b
Ben Wakeford <mail@benwakeford.com>
2023-09-27 17:22:54 +0100

    Basic defaults for container

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <div className="container mx-auto px-4 border-b-2 pb-4 mb-4">
+    <header className="bg-slate-400">
+      <div className="container py-4">
         <h1>My App</h1>
       </div>
     </header>
