604491a617bbb8f868eacc61d4dd55019ad8fd11
Ben Wakeford <mail@benwakeford.com>
2023-09-28 14:37:34 +0100

    Update build process

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+      width: "100%",
+      maxWidth: 1400,
+      padding: 32,
+      screens: {
+        sm: "1400px",
       },
+    },
+    fontFamily: {
+      display: ["Alegreya", "sans-serif"],
+      body: ["Lato", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      primary: "#ee7926",
+      "primary-light": "#fcede1",
+      secondary: "#c2daa0",
+      dark: "#323232",
     },
   },
   plugins: [],
